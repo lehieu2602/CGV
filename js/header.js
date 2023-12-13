d6c43475423a19229660e6ba043a835faf26ee02
hieu.le <hieulm.262@gmail.com>
2023-12-13 21:29:47 +0700

    hoàn thành footer

--- js/header.js
+++ js/header.js
@@ -2,12 +2,20 @@ var currentCell = null;
 
 function toggleList(listId, cell) {
   var list = document.getElementById(listId);
+
   if (list) {
     var isListVisible = list.style.display === "block";
+
+    console.log(isListVisible);
 
     var allLists = document.querySelectorAll(".ul-res");
     allLists.forEach(function (l) {
       l.style.display = "none";
+    });
+
+    var allLists = document.querySelectorAll(".ul-res li");
+    allLists.forEach(function (l) {
+      l.style.display = "block";
     });
 
     if (currentCell) {
@@ -20,3 +28,19 @@ function toggleList(listId, cell) {
     currentCell = cell;
   }
 }
+
+window.addEventListener("resize", function () {
+  var allLists = document.querySelectorAll(".ul-res");
+  allLists.forEach(function (l) {
+    l.style.display = "none";
+  });
+
+  var allLists = document.querySelectorAll(".ul-res li");
+  allLists.forEach(function (list) {
+    list.style.display = "none";
+  });
+
+  if (currentCell) {
+    currentCell.style.backgroundColor = "";
+  }
+});
